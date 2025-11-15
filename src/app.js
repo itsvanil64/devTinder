2332b57bb77ef0dc0c3c6d00a4bf3d86f5d912b3
vanshika <itsvanil64@gmail.com>
2025-11-15 15:04:19 +0530

    playing with multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const app = express();
 
 // app.use('/', (req,res)=> {
 //     res.send("hello from the dashboard");
@@ -22,33 +22,58 @@ const app= express();
 //     res.send({firstName:'vanshika', lastName:'jaiswal'})
 // });
 
-app.get('/user/:userid/:name/:pass', (req,res)=>{
-    console.log(req.params);
-    res.send({firstName:'vanshika', lastName:'jaiswal'})
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route user");
+    //res.send("Response1!");
+    next();
+    // res.send("Response1!");
+  },
+  (req, res,next) => {
+    console.log("handling the route user frm second route");
+   // res.send("Response2!");
+   next();
+  },
+  (req, res,next) => {
+    console.log("handling the route user frm 3rd route");
+    //res.send("Response3!");
+    next();
+  },
+  (req, res,next) => {
+    console.log("handling the route user frm forthroute");
+    res.send("Response4!"); // if not handled here then goes in infinite route
+    //next(); gives the error becoz it does not find any route.
+  }
+);
 
-app.get('/*fly$/', (req,res)=>{
-    res.send({firstName:'vanshika', lastName:'jaiswal'})
-});
+// app.get('/user/:userid/:name/:pass', (req,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:'vanshika', lastName:'jaiswal'})
+// });
 
-app.post('/user',(req,res)=>{
-    //saving data to DB
-    res.send("successfully saved to DB");
-});
+// app.get('/*fly$/', (req,res)=>{
+//     res.send({firstName:'vanshika', lastName:'jaiswal'})
+// });
 
-app.delete('/user',(req,res)=>{
-    //saving data to DB
-    res.send("deleted db successfully");
-});
+// app.post('/user',(req,res)=>{
+//     //saving data to DB
+//     res.send("successfully saved to DB");
+// });
 
-app.use('/test',(req,res) => {
-    res.send("hello from the test");
-});
+// app.delete('/user',(req,res)=>{
+//     //saving data to DB
+//     res.send("deleted db successfully");
+// });
+
+// app.use('/test',(req,res) => {
+//     res.send("hello from the test");
+// });
 
 // app.use('/', (req,res)=> {
 //     res.send("hello from the dashboard");
 // });
 
-app.listen(3000, ()=> {
-    console.log("server is successfully listening om port 3000");
+app.listen(3000, () => {
+  console.log("server is successfully listening om port 3000");
 });
